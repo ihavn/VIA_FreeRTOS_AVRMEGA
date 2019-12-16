8841ed433ef6d61c11472f01255aeda7d7a45ba1
Ib Havn <iha@via.dk>
2019-12-16 08:23:30 +0100

    Change to heap4.c

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -53,6 +53,7 @@
 #define configUSE_TRACE_FACILITY            1
 #define configUSE_16_BIT_TICKS              1
 #define configIDLE_SHOULD_YIELD             1
+#define configTOTAL_HEAP_SIZE				4000
 
 #define configUSE_MUTEXES                   1
 #define configUSE_RECURSIVE_MUTEXES         1
